3062a869f2a56883cf88a528c1b533fac923670c
jefrisaputra <jefrisaputra7373@gmail.com>
2024-10-06 10:46:36 +0700

    FIX Consonant

--- src/main/java/org/nawadatasoution/SortCharacter_NDL010.java
+++ src/main/java/org/nawadatasoution/SortCharacter_NDL010.java
@@ -1,34 +1,59 @@
 package org.nawadatasoution;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SortCharacter_NDL010 {
     public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                String input = scan.nextLine().toLowerCase().replace(" ", "");
+                String vowel = "";
+                String consonant = "";
 
-        Scanner inputan = new Scanner(System.in);
-        System.out.print("Input one line of words (S) :");
-        String data = inputan.nextLine();
-        String vowels = "";
-        String consonants = "";
-
-        String vowelChars = "aeiou";
-
-        for (int i = 0; i < data.length(); i++) {
-            char currentChar = data.charAt(i);
-
-            if (Character.isLetter(currentChar)) {
-                if (vowelChars.contains(String.valueOf(currentChar).toLowerCase())) {
-                    vowels += currentChar;
-                } else {
-                    consonants += currentChar;
+                for(int i = 0; i < input.length(); ++i) {
+                    char c = input.charAt(i);
+                    if (isVowel(c)) {
+                        vowel = vowel + c;
+                    } else {
+                        consonant = consonant + c;
+                    }
                 }
+
+                System.out.println("Vowel Characters :");
+                System.out.println(sortCharsByPosition(vowel));
+                System.out.println("Consonant Characters :");
+                System.out.println(sortCharsByPosition(consonant));
+            }
+
+            public static boolean isVowel(Character c) {
+                return c == 'a' || c == 'i' || c == 'u' || c == 'e' || c == 'o';
+            }
+
+            public static String sortAscByNumericalOrder(String word) {
+                char[] charArray = word.toCharArray();
+                Arrays.sort(charArray);
+                String sortedString = new String(charArray);
+                return sortedString;
+            }
+
+            public static String sortCharsByPosition(String str) {
+                StringBuilder sb = new StringBuilder();
+                char[] charArray = str.toCharArray();
+
+                for(int i = 0; i < charArray.length; ++i) {
+                    char c = charArray[i];
+                    if (str.contains(String.valueOf(c)) && !sb.toString().contains(String.valueOf(c))) {
+                        for(int j = i + 1; j < charArray.length; ++j) {
+                            if (charArray[j] == c) {
+                                sb.append(charArray[j]);
+                            }
+                        }
+
+                        sb.append(c);
+                    }
+                }
+
+                return sb.toString();
+
             }
         }
-
-        System.out.println("Vowel Characters : ");
-        System.out.println(vowels.toLowerCase());
-        System.out.println("Consonant Characters : ");
-        System.out.println(consonants.toLowerCase());
-
-    }
-}
